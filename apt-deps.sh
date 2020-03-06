9657a9e61d539b4e30d5dbd54829ca8602f15922
sigtot <sigurdtotland@gmail.com>
2020-03-06 22:43:13 +0100

    Add highly essential deps

--- apt-deps.sh
+++ apt-deps.sh
@@ -6,3 +6,10 @@ sudo apt install git xorg libx11-dev libxft-dev libxinerama-dev xdm suckless-too
 # Ranger
 sudo apt install ranger
 
+# Other stuff needed to make scripts work
+sudo apt install feh maim xclip zathura
+
+
+# Latex deps
+sudo apt install entr # For the auto-compiler
+sudo apt install texlive-latex-base texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra texlive-science
